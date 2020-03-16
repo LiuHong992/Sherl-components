b2e95fa28a87f2050391095e02142eb096a0afec
DESKTOP-O936DB6\asus <992049747@qq.com>
2020-03-16 17:21:30 +0800

    3-16

--- src/component/Notifyicon/View.vue
+++ src/component/Notifyicon/View.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <notify-icon></notify-icon>
+  <div class="p20-box">
+    <!-- 通知图标 -->
+    <h2 class="m-b-10">通知图标</h2>
+    <div class="notify-icons fl a-c">
+      <!-- 消息为空时 -->
+      <div class="no-msgs fl m-r-20 a-c">
+        <h4>消息为空</h4>
+        <notify-icon></notify-icon>
+      </div>
+      <!-- 消息小于100时 -->
+      <div class="less-100 fl m-r-20 a-c">
+        <h4>消息小于100</h4>
+        <notify-icon>48</notify-icon>
+      </div>
+      <!-- 消息大于100小于1000 -->
+      <div class="less-1000 fl m-r-20 a-c">
+        <h4>消息大于100小于1000</h4>
+        <notify-icon>648</notify-icon>
+      </div>
+      <!-- 消息大于1000 -->
+      <div class="more-1000 fl a-c">
+        <h4>消息大于1000</h4>
+        <notify-icon>6666</notify-icon>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/component/Notifyicon/notifyicon.vue
+++ src/component/Notifyicon/notifyicon.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>通知图标</div>
+  <div class="icon-number p-r f-s-22">
+    <i class="el-icon-bell"></i>
+    <!-- 通知数大于0小于100 -->
+    <div
+      class="numbers p-a f-s-12 wh-text h-20 l-h-20 t-center"
+      v-if="noticeNum>0 && noticeNum<100"
+    >{{noticeNum}}</div>
+    <!-- 通知数大于100小于1000 -->
+    <div
+      class="numbers p-a f-s-12 wh-text h-20 l-h-20 t-center"
+      v-if="noticeNum>=100 && noticeNum<1000"
+    >99</div>
+    <!-- 通知数大于1000 -->
+    <el-tooltip effect="dark" :content="`您有${noticeNum}条消息`" placement="top" v-if="noticeNum>=1000">
+      <div class="numbers mores p-a f-s-12 wh-text h-20 l-h-20 t-center">···</div>
+    </el-tooltip>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      // 用来接收通知条数的参数
+      noticeNum: 0
+    };
   },
   components: {},
   methods: {},
-  mounted() {},
+  mounted() {
+    if (this.$slots.default) {
+      this.noticeNum = this.$slots.default[0].text;
+    }
+  },
   watch: {},
   computed: {},
   filters: {}
@@ -17,4 +40,24 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+.icon-number {
+  cursor: pointer;
+  .numbers {
+    top: -6px;
+    left: 12px;
+    width: 20px;
+    border-radius: 50%;
+    background: #ed4014;
+  }
+  .mores {
+  }
+  .hover-show {
+    top: -80px;
+    background: #303133;
+    margin-bottom: 12px;
+    border-radius: 4px;
+    .trigle {
+    }
+  }
+}
 </style>
